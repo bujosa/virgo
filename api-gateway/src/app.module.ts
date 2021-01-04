4fe74ce988e5e4fa30a5f9d9816b044499cb205d
bujosa <davidbujosa@gmail.com>
2021-01-04 01:08:12 -0400

    finish api-gateway

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLGatewayModule } from '@nestjs/graphql';
+import { gqlModuleInit } from '../config/graphql-gateway-module-initializer';
+
 @Module({
-  imports: [],
+  imports: [
+    ,
+    ConfigModule.forRoot({
+      envFilePath: `./env/.api-gateway.env`,
+    }),
+    GraphQLGatewayModule.forRootAsync({
+      useFactory: () => gqlModuleInit(),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
