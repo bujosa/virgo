8e177763ce4957e373af4d165f57b74c20a00405
David Bujosa <davidbujosa@gmail.com>
2022-02-28 11:24:20 +0100

    fix: add new config on prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,29 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', argsIgnorePattern: '^_' }
+    ],
+    'no-console': 'warn'
+  }
 };
